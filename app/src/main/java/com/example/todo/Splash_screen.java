91a3fc3d50f212a86cbcebbaffe59fd758706e2b
Vaibhav2002 <vaibhavsam2511@gmail.com>
2020-07-18 21:36:37 +0530

    first push

---
+++ app/src/main/java/com/example/todo/Splash_screen.java
@@ -0,0 +1,23 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash_screen.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
